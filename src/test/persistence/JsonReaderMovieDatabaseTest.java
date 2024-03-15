184be271574846934cbff2c6eece2a530705824a
Jugaad <jlally@students.cs.ubc.ca>
2024-03-14 18:46:09 -0700

    Phase 3: JsonTestDatabase reader updates

--- src/test/persistence/JsonReaderMovieDatabaseTest.java
+++ src/test/persistence/JsonReaderMovieDatabaseTest.java
@@ -39,8 +39,8 @@ class JsonReaderMovieDatabaseTest extends JsonTestMovieDatabase {
             MovieDatabase md = reader.read();
             List<Movie> movies = md.getMovies();
             assertEquals(2, movies.size());
-            checkMovie("THE BATMAN", 2022, movies.get(0), 0);
-            checkMovie("BARBIE", 2023, movies.get(1), 0);
+            checkMovie("THE BATMAN", 2022, movies.get(0), 3);
+            checkMovie("BARBIE", 2023, movies.get(1), 1);
         } catch (IOException e) {
             fail("Couldn't read from file");
         }
